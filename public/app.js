ce6597b33085d6089a3ca5fb5e65c9a108cfab7b
Sonny Hu <Sonny.hu@toulouse.miage.fr>
2019-03-18 14:54:49 +0100

    User connection angular js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,20 @@
 var myApp = angular.module('myApp', ['ngResource', 'ngRoute']);
 
-myApp.controller('myAppController', function ($scope, UserFactory) {
+myApp.controller('myAppController', function ($scope, $window, UserFactory) {
+  $scope.creerCompte = function() {
+    var compteToSave = new UserFactory();
+    compteToSave.nom = $scope.nom;
+    compteToSave.prenom = $scope.prenom;
+    compteToSave.adresse = $scope.adresse;
+    compteToSave.mail = $scope.mail;
+    compteToSave.tel = $scope.tel;
+    compteToSave.$save(function(compteToSave) {
+      $scope.resultat = compteToSave;
+      $window.location.href = '/compteCree';
+    }, function(error) {
+      $scope.resultat = error.data.error;
+    });
+  };
   //GET
   $scope.getClient = function() {
     $scope.client = new UserFactory.getMethod({id:$scope.id});
@@ -14,28 +28,26 @@ myApp.controller('myAppController', function ($scope, UserFactory) {
 
 myApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
+  .when('/compteCree', {
+    templateUrl: 'views/CompteCree.html',
+    controller: 'myAppController'
+  })
   .when('/', {
       templateUrl: 'views/presentation.html',
       controller: 'myAppController'
-    })
-    .when('/login', {
+  })
+  .when('/login', {
       templateUrl: 'views/login.html',
       controller: 'myAppController'
-    })
-    .when('/creer', {
+  })
+  .when('/creer', {
       templateUrl: 'views/creerCompte.html',
       controller: 'myAppController'
-    })
+  })
 }]);
 
 myApp.factory('UserFactory', ['$resource', function($resource) {
-  return $resource('/user/:id', {}, {
-    getMethod:  {
-      method: 'GET',
-    },
-    postMethod: {
-      url : '/user/',
-      method: 'POST',
-    } 
+  return $resource('/user/:id', {id: '@_id'}, {
+
   });
 }]);
